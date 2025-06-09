eb9a5a403463fd65854e5646adf54bff251f5cc5
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-09 21:27:57 +0530

    problem solved

---
+++ sort-even-and-odd-indices-independently.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def sort_even_odd_cnt(nums):
+        n = len(nums)
+        even_count = [0] * 101
+        odd_count = [0] * 101
+
+        for i, v in enumerate(nums):
+            if i % 2 == 0:
+                even_count[v] += 1
+            else:
+               odd_count[v] += 1
+
+        res = [0] * n
+        e_idx, o_idx = 0, 1
+
+    # Ascending even positions
+        for val in range(101):
+            while even_count[val] > 0:
+                res[e_idx] = val
+                e_idx += 2
+                even_count[val] -= 1
+
+    # Descending odd positions
+        for val in range(100, -1, -1):
+            while odd_count[val] > 0:
+                res[o_idx] = val
+                o_idx += 2
+                odd_count[val] -= 1
+
+        return res
