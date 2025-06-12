d2b9e7df58b28cc91da4db25be0dd0a1e4baadcb
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-12 20:57:10 +0530

    water solved

---
+++ IBM/waterbottle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def numWaterBottles(self,numBottles, numExchange):
+        total = numBottles
+        empty = numBottles
+
+        while empty >= numExchange:
+            new_bottles = empty // numExchange
+            total += new_bottles
+            empty = new_bottles + (empty % numExchange)
+
+        return total
