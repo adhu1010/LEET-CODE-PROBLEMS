ac8607a71df0f5d25b1c3aa9b70cb3a1922e8cd5
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-15 21:35:24 +0530

    merge intervals

---
+++ IBM/merge intervals.py
@@ -0,0 +1,19 @@
+class Solution (object):
+    
+    def merge(self, intervals):
+        if not intervals:
+            return []
+
+        # Step 1: Sort intervals by the starting time
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+
+        for interval in intervals:
+            # if merged is empty or no overlap
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                # there is overlap, so merge the current and previous
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
