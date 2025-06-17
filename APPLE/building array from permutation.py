d34a39a54205ce103914e351365d2dfc066f6111
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-17 22:11:48 +0530

    time complexities added

--- APPLE/building array from permutation.py
+++ APPLE/building array from permutation.py
@@ -1,3 +1,6 @@
 class Solution(object):
     def buildArray(self, nums: List[int]) -> List[int]:
         return [nums[nums[i]] for i in range(len(nums))]
+
+#Time Complexity: O(n) where n is the length of the input list nums, as we iterate through the list once.
+#Space Complexity: O(n) for storing the resulting list, which has the same length as nums.
