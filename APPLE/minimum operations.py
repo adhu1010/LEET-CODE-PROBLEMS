3dff975d7ccb76da3cac6becf988e9968c616c04
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-17 22:13:12 +0530

    minimum operations

---
+++ APPLE/minimum operations.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minOperations(self, nums: List[int]) -> int:
+        operations = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                increment = nums[i - 1] - nums[i] + 1
+                operations += increment
+                nums[i] += increment
+        return operations
