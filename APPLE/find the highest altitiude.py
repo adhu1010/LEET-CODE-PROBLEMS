d966b051636926ea01225f0a9ed7150b8fadc31c
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-18 19:53:14 +0530

    time comlexities

--- APPLE/find the highest altitiude.py
+++ APPLE/find the highest altitiude.py
@@ -6,3 +6,6 @@ class Solution:
             altitude += g
             max_altitude = max(max_altitude, altitude)
         return max_altitude
+
+# Time Complexity: O(n) where n is the length of the input list gain, as we iterate through the list once.
+# Space Complexity: O(1) since we only use a few variables to keep track of the current altitude and the maximum altitude.
