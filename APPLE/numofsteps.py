abce06c8be3c3894b838b92d79ebd9997d305ea9
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-20 21:14:22 +0530

    time complexities

--- APPLE/numofsteps.py
+++ APPLE/numofsteps.py
@@ -8,3 +8,6 @@ class Solution:
                 num -= 1
             steps += 1
         return steps
+
+# # Time Complexity: O(log n) in the worst case, as the number is halved in each step when it is even.
+# # Space Complexity: O(1) since we are using a constant amount of space for the steps variable.
--- APPLE/run length.py
+++ APPLE/run length.py
@@ -6,3 +6,6 @@ class Solution:
             val = nums[i + 1]
             result.extend([val] * freq)
         return result
+
+# Time Complexity: O(n) where n is the length of the input list nums, as we iterate through the list in steps of 2.
+# Space Complexity: O(n) for the output list, which can be as large as the sum of all frequencies in nums.
